06055234a69f28e9fd209e411a187cf1dbefae04
Shubham-Jadhav11 <jadhav.shubham1805@gmail.com>
2022-01-30 13:58:13 +0530

    initial commit

---
+++ src/test/java/com/marvel/rest/SpringRestClientExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marvel.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestClientExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
